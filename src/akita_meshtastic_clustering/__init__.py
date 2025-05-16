d2d4eca2b7fc8df2640a3f7bc5f6d4b96dff0822
sh4un <97253929+sh4un-dot-com@users.noreply.github.com>
2025-05-16 09:06:38 -0400

    Create __init__.py

---
+++ src/akita_meshtastic_clustering/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2025 Akita Engineering <https://www.akitaengineering.com>
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# This file makes 'akita_meshtastic_clustering' a Python package.
+from .node import Node
+from .config import load_config
+
+__version__ = "0.4.0" # Final version incorporating API guesses/docs
